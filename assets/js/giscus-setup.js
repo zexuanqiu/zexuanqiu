1beda1b257538175b366b63f607635090a8646b3
zexuanqiu <45207683+zexuanqiu@users.noreply.github.com>
2026-01-08 18:07:46 +0000

    Deploying to gh-pages from @ zexuanqiu/zexuanqiu.github.io@78c3e4c380484a304410a1eac29ef4e409a10d49 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "zexuanqiu/zexuanqiu",
+    "data-repo": "zexuanqiu/zexuanqiu.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
